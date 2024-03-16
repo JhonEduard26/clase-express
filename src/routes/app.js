0210881ca278afacc798d45f230a81ded02b4838
jhon <jhon.bocanegra26@gmail.com>
2024-03-16 13:00:45 -0500

    add: password utility and database connection files

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const pool = require('../db/db');
 const router = express.Router();
+const { hashPassword } = require('../utils/password')
 
 router.get('/login', async (req, res) => {
   res.render('./app/login', {
     titulo: 'Inicio de sesión',
   });
+})
+
+router.post('/registro', async (req, res) => {
+  const {
+    nombre,
+    apellido,
+    correo,
+    password,
+    rol,
+    estado
+  } = req.body;
+
+  const passwordHash = await hashPassword(password);
+  console.log(passwordHash);
 })
 
 module.exports = router;
